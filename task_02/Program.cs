115c22db6fb56840048c76c0973eb86f9da704dc
Milamber1990 <andrey.kalva72@gmail.com>
2023-04-24 20:21:29 +0300

    task_02

---
+++ task_02/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int num1 = num % 2;
+
+if(num1 == 0)
+Console.Write("ДА");
+
+if(num1 == 1)
+Console.Write("НЕТ");
