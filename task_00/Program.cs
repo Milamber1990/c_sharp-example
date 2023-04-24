91a887b2c8064ba783eb3421dc5e2b214721d20b
Milamber1990 <andrey.kalva72@gmail.com>
2023-04-24 20:00:32 +0300

    Initial commit

---
+++ task_00/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите первое число ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите второе число ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        if(num1 > num2)
+        Console.Write(num1);
+        else
+        Console.Write(num2);
+    }
+}
