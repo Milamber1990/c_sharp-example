b397b5b5647b860f9f1aa6d5ba62ddbe512fc48a
Milamber1990 <andrey.kalva72@gmail.com>
2023-04-24 20:12:00 +0300

    task_01

---
+++ task_01/Program.cs
@@ -0,0 +1,13 @@
+﻿ Console.Write("Введите первое число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int maximum = num1;
+
+if(num2 > maximum) maximum = num2;
+if(num3 > maximum) maximum = num3;
+
+Console.Write(maximum);
